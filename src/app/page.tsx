1a5c6d14bd71f4ef187190664bc1891938bc44dc
Priyanshu Indra <indrapriyanshu@gmail.com>
2026-09-18 22:10:36 +0530

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-   <h1>Hello world</h1> 
+    <>
+      <h1 className="text-sky-400">Hello world</h1>
+      <Button>Hello World</Button> 
+      <pre>Hello World is the best program ever</pre>
+    </>
+
   );
 }
+
